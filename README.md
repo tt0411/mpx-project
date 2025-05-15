93a484d625dce8889d03ba42b6d7ee0343903dec
wangmengtao <2726653264@qq.com>
2025-05-15 11:10:03 +0800

    feat: 更新请求逻辑，优化加载控制，调整构建命令

--- README.md
+++ README.md
@@ -1,13 +1,23 @@
-# mpx-project
-
-## Project setup
+# mpx-project 跨端小程序框架
 
 ```javascript
-// development
-npm run serve // 小程序本地开发构建
+pnpm i
 
+// development
+pnpm run serve // 小程序本地开发构建
 
 // production
-npm run build // 小程序生产环境构建
+pnpm run build // 小程序生产环境构建
 
+```
+
+注意:
+打包阿里小程序 uno.acss 文件会自动是生成默认样式
+```css
+  button {
+    height: initial; // 这里要删除，否则van-button 按钮高度样式会失效
+    border-radius: 5px;
+    box-sizing: border-box;
+    cursor: pointer;
+ }    
 ```
--- src/api/request.js
+++ src/api/request.js
@@ -1,109 +1,156 @@
-import mpx from '@mpxjs/core'
+import mpx from "@mpxjs/core";
 
-const baseURL = 'http://xxx.com'
-let loadingCount = 0
+const baseURL =
+  process.env.NODE_ENV === "development"
+    ? "https://xxx.dev.xxx.com" // 测试环境
+    : "https://xxx.xxx.com"; // 线上环境
+let loadingCount = 0;
 
 // 全局 loading 控制
 function showLoading() {
   if (loadingCount === 0) {
-    mpx.showLoading({ title: '加载中...' })
+    mpx.showLoading({ title: "加载中..." });
   }
-  loadingCount++
+  loadingCount++;
 }
 function hideLoading() {
-  loadingCount--
+  loadingCount--;
   if (loadingCount <= 0) {
-    loadingCount = 0
-    mpx.hideLoading()
+    loadingCount = 0;
+    mpx.hideLoading();
   }
 }
 
 // 添加请求拦截器
 mpx.xfetch.interceptors.request.use((config) => {
   if (config.showLoading === true) {
-    showLoading()
+    showLoading();
   }
   // 可在此添加 token 等
-  return config
-})
+  return config;
+});
 
 // 添加响应拦截器
 mpx.xfetch.interceptors.response.use(
   (response) => {
-    if (response.config.showLoading) {
-      hideLoading()
+    console.log(response, "=== response ===");
+    if (response.requestConfig.showLoading) {
+      hideLoading();
     }
-    return response
+    if (response.data.code !== 0) {
+      if (response.requestConfig.showToastMsg === true) {
+        mpx.showToast({
+          title: response.data.message || "网络异常",
+          icon: "none",
+        });
+      }
+      return Promise.reject(response.data);
+    }
+    return response.data;
   },
   (error) => {
-    hideLoading()
-    mpx.showToast({ title: error.message || '网络错误', icon: 'none' })
-    return Promise.reject(error)
+    hideLoading();
+    if (error.code === "ECONNABORTED") {
+      mpx.showToast({ title: "请求超时", icon: "error" });
+      return Promise.reject(error);
+    }
+    try {
+      const { status, data } = error.response;
+      let message;
+      if (status === 404) {
+        message = `接口【${data.path}】未定义`;
+      } else if (status === 500) {
+        message = `服务不可用`;
+      } else {
+        message = `系统异常`;
+      }
+      mpx.showToast({ title: message, icon: "error" });
+      return Promise.reject(error);
+    } catch (err) {
+      return Promise.reject(err);
+    }
   }
-)
+);
 
 // 通用请求方法
-function request({ url, config = {}}) {
-  return mpx.xfetch.fetch({
-    url: url.includes('http') ? url : baseURL + url,
-    timeout: config.timeout ?? 10000,
-    ...config,
-  }).then(res => res.data)
+function request({ url, config = {} }) {
+  return mpx.xfetch
+    .fetch({
+      url: url.includes("http") ? url : baseURL + url,
+      ...config,
+      timeout: config.timeout ?? 10000,
+    })
+    .then((res) => res.data);
 }
 
 // GET
 export function get(url, parameter = {}) {
-  const { config = {}, showLoading = false, params } = parameter
-  config.method = 'GET'
-  config.params = params
-  config.showLoading = showLoading
-  config.headers = config.headers || {}
-  config.timeout = config.timeout
-  return request({ url, config })
+  const { config = {}, showLoading, showToastMsg, params } = parameter;
+  config.method = "GET";
+  config.params = params;
+  config.showLoading = showLoading;
+  config.showToastMsg = showToastMsg;
+  config.headers = config.headers || {};
+  config.timeout = config.timeout;
+  return request({ url, config });
 }
 
 // POST
 export function post(url, parameter = {}) {
-  const { config = {}, showLoading = false, data } = parameter
-  config.method = 'POST'
-  config.data = data
-  config.showLoading = showLoading
-  config.headers = config.headers || {}
-  config.timeout = config.timeout
-  return request({ url, config })
+  const { config = {}, showLoading, showToastMsg, data, params } = parameter;
+  config.method = "POST";
+  config.params = params;
+  config.data = data;
+  config.showLoading = showLoading;
+  config.showToastMsg = showToastMsg;
+  config.headers = config.headers || {};
+  config.timeout = config.timeout;
+  return request({ url, config });
 }
 
 // PUT
 export function put(url, parameter = {}) {
-  const { config = {}, showLoading = false, data } = parameter
-  config.method = 'PUT'
-  config.data = data
-  config.headers = config.headers || {}
-  config.timeout = config.timeout
-  config.showLoading = showLoading
-  return request({ url, config })
+  const { config = {}, showLoading, showToastMsg, data, params } = parameter;
+  config.method = "PUT";
+  config.data = data;
+  config.params = params;
+  config.showLoading = showLoading;
+  config.showToastMsg = showToastMsg;
+  config.timeout = config.timeout;
+  config.headers = config.headers || {};
+  return request({ url, config });
 }
 
 // DELETE
 export function del(url, parameter = {}) {
-    const { config = {}, showLoading = false, data } = parameter
-    config.method = 'DELETE'
-    config.data = data
-    config.showLoading = showLoading
-    config.headers = config.headers || {}
-    config.timeout = config.timeout
-    return request({ url, config })
+  const { config = {}, showLoading, showToastMsg, data, params } = parameter;
+  config.method = "DELETE";
+  config.data = data;
+  config.params = params;
+  config.headers = config.headers || {};
+  config.showLoading = showLoading;
+  config.showToastMsg = showToastMsg;
+  config.timeout = config.timeout;
+  return request({ url, config });
 }
 
 // 上传文件
 export function uploadFile(url, parameter = {}) {
-  const { config = {}, formData = {}, showLoading = false } = parameter
-  config.method = 'POST'
+  const {
+    config = {},
+    formData = {},
+    params,
+    showLoading,
+    showToastMsg,
+  } = parameter;
+  config.method = "POST";
   config.headers = {
-    'Content-Type': 'multipart/form-data',
-  }
-  config.formData = formData
-  config.timeout = config.timeout
-  config.showLoading = showLoading
-  return request({ url, config })
+    "Content-Type": "multipart/form-data",
+  };
+  config.formData = formData;
+  config.params = params;
+  config.showToastMsg = showToastMsg;
+  config.timeout = config.timeout;
+  config.showLoading = showLoading;
+  return request({ url, config });
 }
